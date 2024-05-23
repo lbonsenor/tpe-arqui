2117bd3245f03de1c463b3271546acc1d72a9890
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-05-23 15:43:46 -0300

    Added functions from tp5

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,4 +1,8 @@
 GLOBAL cpuVendor
+GLOBAL bringMinutes
+GLOBAL bringHours
+GLOBAL bringSeconds
+GLOBAL whatKeyPressed
 
 section .text
 	
@@ -25,3 +29,70 @@ cpuVendor:
 	mov rsp, rbp
 	pop rbp
 	ret
+
+
+bringSeconds:   
+            cli
+            ;config del número n binario
+            mov al, 0x0B 
+            out 70h, al
+            in al, 71h
+            or al,4 
+            out 71h, al 
+
+
+            ;lectura de los segundos
+            mov al, 0x00
+            out 70h, al
+            in al, 71h
+            sti
+
+            ret
+bringMinutes:
+            cli
+            ;config del numero n binario
+            mov al, 0x0B 
+            out 70h, al
+            in al, 71h
+            or al,4 
+            out 71h, al 
+
+            ;lectura de los minutos 
+            mov al, 0x02
+            out 70h, al
+            in al, 71h
+            sti
+
+            ret
+bringHours:
+            cli
+
+            ;config del número n binario
+            mov al, 0x0B 
+            out 70h, al
+            in al, 71h
+            or al,4 
+            out 71h, al 
+
+            ;lectura de los minutos
+            mov al, 0x04
+            out 70h, al
+            in al, 71h
+            sti
+
+            ret
+
+whatKeyPressed: 
+        ;make sure register is empty
+        mov rax, 0
+    .inicio: 
+        ;64h status register
+        in al, 64h
+        ;does signal status port have data for system?
+        and al, 0x01
+        ;if not, return to cycle
+        je .inicio
+        ;else read the data
+        in al, 60h
+
+        ret
