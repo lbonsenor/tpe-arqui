43afa584cea57a5111f3f20f4edd933aa9ddc02e
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-05-23 19:22:16 -0300

    printKey detects ONE pressed key and prints it

--- Kernel/keyboardDriver.c
+++ Kernel/keyboardDriver.c
@@ -1,5 +1,6 @@
 extern int getKey();
 
+
 void printKey() {
     // Key map
     char kbd_US [128] =
@@ -39,9 +40,9 @@ void printKey() {
     int scancodeKey = getKey();
     char key[2] = {kbd_US[scancodeKey], 0};
     // Unknown key
-    if(kbd_US[scancodeKey] == 0) print("Unknown key", 0x72, 0xB8000 + 40 * 2);
+    if(kbd_US[scancodeKey] == 0) print(0x00159854, "Unknown key", 10);
     // Print key
-    else print(key, 0x72, 0xB8000 + 40 * 2);
+    else print(0x00159854, key, 10);
     // Execute constantly
     while(1);
 }
