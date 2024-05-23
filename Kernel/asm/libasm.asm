e316621f6521180d60b7341ddb8b0c4bd37734fe
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-05-23 19:12:19 -0300

    Adding support for scanning keys (not final)

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -25,3 +25,12 @@ cpuVendor:
 	mov rsp, rbp
 	pop rbp
 	ret
+
+getKey: 
+    mov rax, 0
+.start: 
+    in al, 64h      ; Reads keyboard status  
+    and al, 0x01    ; If there isn't any data to read, restart
+    je .start
+    in al, 60h      ; If there is data, get the scancode of the pressed key
+    ret
---
+++ Kernel/keyboardDriver.c
@@ -0,0 +1,47 @@
+extern int getKey();
+
+void printKey() {
+    // Key map
+    char kbd_US [128] =
+    {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',   
+  '\t', /* <-- Tab */
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',     
+    0, /* <-- control key */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+    };
+    int scancodeKey = getKey();
+    char key[2] = {kbd_US[scancodeKey], 0};
+    // Unknown key
+    if(kbd_US[scancodeKey] == 0) print("Unknown key", 0x72, 0xB8000 + 40 * 2);
+    // Print key
+    else print(key, 0x72, 0xB8000 + 40 * 2);
+    // Execute constantly
+    while(1);
+}
