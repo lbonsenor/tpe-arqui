f778ee4335ae8272471392caf8431df2700017ac
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-23 20:23:49 -0300

    Fix

--- Kernel/keyboardDriver.c
+++ Kernel/keyboardDriver.c
@@ -1,58 +1,34 @@
 extern int getKey();
 #include <keyboardDriver.h>
 
-#define isVisibleChar(a) 
+#define BUFFER_SIZE 1024
+static const char kbd_US[256] = {
+   /* 0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', '+', '\n',
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+    0, 0, 0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 0, 0, 0, 0 */       
+        0,  1/*esc*/,  '1',  '2',  '3',  '4',  '5',  '6',   '7',  '8',  '9',   '0',   '\'',  '<', '\b',
+    '\t', 'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',   'o',  'p',  '\\',   '+', '\n',   
+        0,     'a',  's', 'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  '{',
+    '|',  5/*shift*/,  '}',  'z',  'x', 'c', 'v',  'b',  'n',  'm',  ',',  '.',  '-',    0,  
+    '*',     0,  ' ',    0,     0,     0/*60*/,    0,       0,         0, 
+    0,     0,     0,    0,      0,      0,      0,      0,         17/*up*/,
+    0,      0,      18/*left*/,    0,     19/*right*/,     0,      0,      20/*down*/,      0,
+};
 
 void printKey() {
-    // Key map
-    char kbd_US [128] =
-    {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',   
-  '\t', /* <-- Tab */
-  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',     
-    0, /* <-- control key */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,
-  '*',
-    0,  /* Alt */
-  ' ',  /* Space bar */
-    0,  /* Caps lock */
-    0,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
-  '-',
-    0,  /* Left Arrow */
-    0,
-    0,  /* Right Arrow */
-  '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
-    };
-    
     char flag = 0;
     char buffer[1024] = {0};
     int currentChar = 0;
     while (!flag)
     {
-      char shiftFlag = 0;
       int scancodeKey = getKey();
       
-      char key[2] = {kbd_US[scancodeKey], 0};
+      char key = kbd_US[scancodeKey];
       // Unknown key
       if(kbd_US[scancodeKey] == 0);
       else {
-        switch (key[0])
+        switch (key)
         {
         case '\n':
           flag = 1;
@@ -60,9 +36,9 @@ void printKey() {
         case 0:
           break;
         default:
-          if (key[0] < 126 && key[0] > 0)
+          if (key < 126 && key > 0)
           {
-            buffer[currentChar++] = key[0];
+            buffer[currentChar++] = key;
           }
           
           break;
