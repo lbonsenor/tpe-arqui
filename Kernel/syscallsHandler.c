5012a364f7b1aa228b6eede0f356ce17a625f62d
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-05-27 15:46:34 -0300

    error solved

--- Kernel/syscallsHandler.c
+++ Kernel/syscallsHandler.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
 // luego hacer cases segun ll que recibo en rax...
-uint64_t syscallHandler( uint64_t rdi, u_int64_t rsi , uint64_t rdx , uint64_t r10, uint64_t r8 , uint64_t rax ){
+uint64_t syscallHandler(uint64_t rdi, uint64_t rsi , uint64_t rdx , uint64_t r10, uint64_t r8 , uint64_t rax ){
     return 0;
 }
 
