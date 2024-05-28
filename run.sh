e7fcd9883b057422c75b18be49e2b017511f77b4
felitba <feli@itba.edu.ar>
2024-05-27 21:04:10 -0300

    just added two more parameters in order to make de run.sh work with the soundDriver

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #make clean
 #make all
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev $audio,id=audio0 -soundhw pcspk
