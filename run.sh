f1cef8c87301235c213c01049eadbd94f1921f43
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-29 18:23:44 -0300

    for mac

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
+if [[ "$OSTYPE" == *"darwin"*]]; then
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
+
