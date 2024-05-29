f715103b088097f00dcf6ba22fd6a0ece75a7a06
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-29 16:28:55 -0300

    compile.sh for make

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
