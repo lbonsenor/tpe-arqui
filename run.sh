444778d18e45b0260a726cd54198bb26308dba3d
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-29 18:55:20 -0300

    correction 2: electric bogaloo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$OSTYPE" =~ "*darwin*" ]]; then
+if [[ "$OSTYPE" =~ "darwin" ]]; then
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
 else
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
