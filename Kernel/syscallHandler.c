50ddda1d8d9aa07007d788017d5d342d168cd565
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-05-29 20:14:28 -0300

    Added syscalls to code switch

--- Kernel/syscallHandler.c
+++ Kernel/syscallHandler.c
@@ -14,27 +14,57 @@
 extern const uint64_t show_registers_dump[17];
 extern const uint64_t has_regs;
 
+// syscalls params:	RDI	RSI	RDX	R10	R8	R9
+// syscallHandler:	RDI RSI RDX R10 R8  RAX
+// params in C are:	RDI RSI RDX RCX R8  R9
+
 uint64_t syscallHandler(uint64_t rdi, uint64_t rsi , uint64_t rdx , uint64_t r10, uint64_t r8 , uint64_t rax) {
     switch (rax){
         case 0:
+            return read(rdi, rsi , rdx);
         case 1:
+            return write(rdi,rsi,rdx);
         case 2:
+            return time();
         case 3:
+            return elapsed_millis();
         case 4:
+            return get_height_ch();
         case 5:
+            return get_width_ch();
         case 6:
+            clear_line(rdi);
+            break;
         case 7:
+            clear_screen();
+            break;
         case 8:
+            return put_pixel(rdi,rdi,rdx);
         case 9:
+            return draw_rect(rdi,rsi,rdx,r10,r8);
         case 10:
+            return scale_up();
         case 11:
+            return scale_down();
         case 12:
+            return make_sound(rdi,rsi,rdx);
         case 13:
+            return get_height_pix();
         case 14:
+            return get_width_pix();
         case 15:
+            return get_pix(rdi,rsi);
         case 16:
-        case 17:
+            return get_max_lines();
+            break;
+        case 17: 
+            set_cursor_to_line(rdi);
+            break;
+        case 18:
+            return save_registers(rdi);
         case 35:
+            wait(rdi);
+            break;
         default:
             return 1;
     }
@@ -92,7 +122,7 @@ static uint64_t write(uint64_t fileDescriptor, uint64_t buffer , uint64_t length
     }
 }
 
-static uint64_t time (){
+static uint64_t time(){
     return getTime();
 }
 
@@ -108,7 +138,7 @@ static uint64_t get_width_ch(){
     return getWidthChars();
 }
 
-static uint64_t clear_line(uint64_t line){
+static void clear_line(uint64_t line){
     int startY = lineToHeight(line); 
     for(int i = 0; i< getWidthPixels(); i++){
         int heightCounter=0;
@@ -116,12 +146,10 @@ static uint64_t clear_line(uint64_t line){
             putPixel(0x000000, i , heightCounter+startY);
         }
     }
-    return 0;
 }
 
-static uint64_t clear_screen(){
+static void clear_screen(){
     clearScreen();
-    return 0;
 }
 
 static uint64_t put_pixel(uint64_t color , uint64_t x, uint64_t y){
