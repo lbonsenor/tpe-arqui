1c54420b5cc32d006314f122e061a5f2cd49bb13
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-05-28 23:44:17 -0300

    more small changes

--- Kernel/moduleLoader.c
+++ Kernel/moduleLoader.c
@@ -6,20 +6,15 @@
 static void loadModule(uint8_t ** module, void * targetModuleAddress);
 static uint32_t readUint32(uint8_t ** address);
 
-void loadModules(void * payloadStart, void ** targetModuleAddress)
-{
+void loadModules(void * payloadStart, void ** targetModuleAddress) {
 	int i;
 	uint8_t * currentModule = (uint8_t*)payloadStart;
 	uint32_t moduleCount = readUint32(&currentModule);
-
-	for (i = 0; i < moduleCount; i++)
-		loadModule(&currentModule, targetModuleAddress[i]);
+	for (i = 0; i < moduleCount; i++) loadModule(&currentModule, targetModuleAddress[i]);
 }
 
-static void loadModule(uint8_t ** module, void * targetModuleAddress)
-{
+static void loadModule(uint8_t ** module, void * targetModuleAddress) {
 	uint32_t moduleSize = readUint32(module);
-
 	ncPrint("  Will copy module at 0x");
 	ncPrintHex((uint64_t)*module);
 	ncPrint(" to 0x");
@@ -27,16 +22,13 @@ static void loadModule(uint8_t ** module, void * targetModuleAddress)
 	ncPrint(" (");
 	ncPrintDec(moduleSize);
 	ncPrint(" bytes)");
-
 	memcpy(targetModuleAddress, *module, moduleSize);
 	*module += moduleSize;
-
 	ncPrint(" [Done]");
 	ncNewline();
 }
 
-static uint32_t readUint32(uint8_t ** address)
-{
+static uint32_t readUint32(uint8_t ** address) {
 	uint32_t result = *(uint32_t*)(*address);
 	*address += sizeof(uint32_t);
 	return result;
