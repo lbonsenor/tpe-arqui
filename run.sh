252e2bfb3712d6cb88ec503b176b83fe72a3d723
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-31 23:17:41 -0300

    tgdb

--- run.sh
+++ run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 if [[ "$OSTYPE" =~ "darwin" ]]; then
-    if [ "$1" = "-d" ]; then
-        qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
-    else
-        qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
-    fi
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
 else
-    if [ "$1" = "-d" ]; then
-        qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+    if [[ "$1" = "-d" ]]; then
+        qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
     else
-        qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+        qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
     fi
 fi
-
