863a6d5306b22238887ddf83235fa5f46c6b492b
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-05-31 22:58:17 -0300

    prepared run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 if [[ "$OSTYPE" =~ "darwin" ]]; then
-    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+    if [ "$1" = "-d" ]; then
+        qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+    else
+        qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+    fi
 else
-    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
+    if [ "$1" = "-d" ]; then
+        qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+    else
+        qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+    fi
 fi
+
