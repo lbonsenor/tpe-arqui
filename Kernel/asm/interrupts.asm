66496f9f0fc6f34a61facaf5efc86bdaeeff750e
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-06-02 19:09:06 -0300

    made the registers part better? possibly?

--- Kernel/asm/interrupts.asm
+++ Kernel/asm/interrupts.asm
@@ -194,14 +194,35 @@ _exception6Handler:
 	exceptionHandler 6
 
 _int80Handler:
-	; syscalls params:	RDI	RSI	RDX	R10	R8	R9
-	; syscallHandler:	RDI RSI RDX R10 R8  RAX
-	; params in C are:	RDI RSI RDX RCX R8  R9
-    ; pushState
-    ; mov rcx, r10
-	; mov r9, rax -> we will be calling all params from c so its the same
+  	push rbx
+	push rcx
+	push rdx
+	push rbp
+	push rdi
+	push rsi
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
     call syscallHandler
-    ; popState
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsi
+	pop rdi
+	pop rbp
+	pop rdx
+	pop rcx
+	pop rbx
 	iretq
 
 haltcpu:
